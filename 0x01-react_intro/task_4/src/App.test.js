64320fcd18f608da1aa8d6fdba9bcab0d79e31a1
SurafelFikadu <surafelfikadu05@gmail.com>
2024-07-27 07:18:49 +0300

    ...

--- 0x01-react_intro/task_4/src/App.test.js
+++ 0x01-react_intro/task_4/src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
+import React from 'react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const wrapper = shallow(<App />);
+
+test('checkin crashing issue', function () {
+  shallow(<App />);
+});
+
+test('checkin header', function () {
+  expect(wrapper.find('header.App-header').exists()).toEqual(true);
+});
+test('checkin body', function () {
+  expect(wrapper.find('body.App-body').exists()).toEqual(true);
+});
+test('checkin footer', function () {
+  expect(wrapper.find('footer.App-footer').exists()).toEqual(true);
 });
