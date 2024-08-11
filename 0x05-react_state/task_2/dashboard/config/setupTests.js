3ba90071271402147b8916ff8914fa554408a555
SurafelFikadu <surafelfikadu05@gmail.com>
2024-08-11 14:52:10 +0300

    ...

---
+++ 0x05-react_state/task_2/dashboard/config/setupTests.js
@@ -0,0 +1,7 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
