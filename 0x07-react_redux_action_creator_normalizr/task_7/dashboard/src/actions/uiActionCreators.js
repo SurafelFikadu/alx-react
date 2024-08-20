8e5b0c20c748d3f2d9957403438e7afe605f8c74
SurafelFikadu <surafelfikadu05@gmail.com>
2024-08-20 15:40:14 +0300

    ...

--- 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
+++ 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
@@ -1,4 +1,5 @@
-import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from './uiActionTypes';
+import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS, LOGIN_FAILURE } from './uiActionTypes';
 
 export const login = (email, password) => {
   return { 
@@ -11,17 +12,39 @@ export const login = (email, password) => {
 };
 
 export const boundlogin = (email, password) => dispatch(login(email, password));
+
 export const logout = () => {
     return { type: LOGOUT };
 };
 
 export const boundlogout = () => dispatch(logout());
+
 export const displayNotificationDrawer = () => {
     return { type: DISPLAY_NOTIFICATION_DRAWER };
 };
 
 export const bounddisplayNotificationDrawer = () => dispatch(displayNotificationDrawer());
+
 export const hideNotificationDrawer = () => {
     return { type: HIDE_NOTIFICATION_DRAWER };
 };
+
 export const boundhideNotificationDrawer = () => dispatch(hideNotificationDrawer());
+
+export const loginSuccess = () => {
+  return { type: LOGIN_SUCCESS }
+}
+
+export const loginFailure = () => {
+  return { type: LOGIN_FAILURE }
+}
+
+export const loginRequest = (email, password) => {
+  return function (dispatch) {
+    dispatch(login(email, password));
+    return fetch('http://localhost:3000/login-success.json')
+      .then((response) => response.json())
+      .then((json) => dispatch(loginSuccess()))
+      .catch((err) => dispatch(loginFailure()))
+  };
+}
