756e006940cf142ea9ceff4dc77b4019287c528d
SurafelFikadu <surafelfikadu05@gmail.com>
2024-09-01 18:26:30 -0500

    ...

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/Notifications/Notifications.test.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/Notifications/Notifications.test.js
@@ -3,6 +3,72 @@ import React from "react";
 import Notifications from "./Notifications";
 import { getLatestNotification } from "../utils/utils";
 import { StyleSheetTestUtils } from "aphrodite";
+import notificationsNormalizer from "../schema/notifications";
+import { Map, fromJS } from "immutable";
+import { getUnreadNotificationsByType } from "../selectors/notificationSelector";
+
+const NOTIFICATIONS = [
+  {
+    id: "5debd76480edafc8af244228",
+    author: {
+      id: "5debd764a7c57c7839d722e9",
+      name: {
+        first: "Poole",
+        last: "Sanders",
+      },
+      email: "poole.sanders@holberton.nz",
+      picture: "http://placehold.it/32x32",
+      age: 25,
+    },
+    context: {
+      guid: "2d8e40be-1c78-4de0-afc9-fcc147afd4d2",
+      isRead: true,
+      type: "urgent",
+      value:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+    },
+  },
+  {
+    id: "5debd764507712e7a1307303",
+    author: {
+      id: "5debd7648ba8641ce0a34ea4",
+      name: {
+        first: "Norton",
+        last: "Grimes",
+      },
+      email: "norton.grimes@holberton.nz",
+      picture: "http://placehold.it/32x32",
+      age: 37,
+    },
+    context: {
+      guid: "cec84b7a-7be4-4af0-b833-f1485433f66e",
+      isRead: false,
+      type: "urgent",
+      value:
+        "ut labore et dolore magna aliqua. Dignissim convallis aenean et tortor at risus viverra adipiscing. Ac tortor dignissim convallis aenean et. ",
+    },
+  },
+  {
+    id: "5debd76444dd4dafea89d53b",
+    author: {
+      id: "5debd764a7c57c7839d722e9",
+      name: {
+        first: "Poole",
+        last: "Sanders",
+      },
+      email: "poole.sanders@holberton.nz",
+      picture: "http://placehold.it/32x32",
+      age: 25,
+    },
+    context: {
+      guid: "280913fe-38dd-4abd-8ab6-acdb4105f922",
+      isRead: false,
+      type: "urgent",
+      value:
+        "Non diam phasellus vestibulum lorem sed risus ultricies. Tellus mauris a diam maecenas sed",
+    },
+  },
+];
 
 describe("<Notifications />", () => {
   let listNotifications;
@@ -48,16 +114,37 @@ describe("<Notifications />", () => {
   describe("Notifications with listNotifications", () => {
     beforeEach(() => {
       latestNotification = getLatestNotification();
-      listNotifications = [
-        { id: 1, type: "default", value: "New course available" },
-        { id: 2, type: "urgent", value: "New resume available" },
-        { id: 3, type: "urgent", html: { __html: latestNotification } },
-      ];
+      listNotifications = {
+        notifications: fromJS({
+          messages: {
+            1: {
+              guid: 1,
+              type: "default",
+              value: "New course available",
+              isRead: false,
+            },
+            2: {
+              guid: 2,
+              type: "urgent",
+              value: "New resume available",
+              isRead: false,
+            },
+            3: {
+              guid: 3,
+              type: "urgent",
+              html: { __html: latestNotification },
+              isRead: false,
+            },
+          },
+        }),
+      };
     });
 
     it("Notifications renders Notification Items and items have correct html", () => {
+      const messages = getUnreadNotificationsByType(listNotifications);
+
       const wrapper = mount(
-        <Notifications displayDrawer listNotifications={listNotifications} />
+        <Notifications displayDrawer listNotifications={messages} />
       );
       expect(wrapper.exists());
       wrapper.update();
@@ -97,9 +184,7 @@ describe("<Notifications />", () => {
     });
 
     it("Notifications renders Notification Item correct with empty listNotifications", () => {
-      const wrapper = mount(
-        <Notifications displayDrawer listNotifications={listNotifications} />
-      );
+      const wrapper = mount(<Notifications displayDrawer />);
       expect(wrapper.exists());
       wrapper.update();
       const listItems = wrapper.find("NotificationItem");
@@ -229,6 +314,27 @@ describe("<Notifications />", () => {
 
       expect(handleDisplayDrawer).not.toHaveBeenCalled();
       expect(handleHideDrawer).toHaveBeenCalled();
+
+      jest.restoreAllMocks();
+    });
+
+    it("verify that clicking on the menu item calls handleDisplayDrawer", () => {
+      const setNotificationFilter = jest.fn();
+
+      const wrapper = shallow(
+        <Notifications
+          setNotificationFilter={setNotificationFilter}
+          displayDrawer={true}
+        />
+      );
+
+      wrapper.find("#buttonFilterUrgent").simulate("click");
+
+      expect(setNotificationFilter).toHaveBeenNthCalledWith(1, "URGENT");
+
+      wrapper.find("#buttonFilterDefault").simulate("click");
+
+      expect(setNotificationFilter).toHaveBeenNthCalledWith(2, "DEFAULT");
 
       jest.restoreAllMocks();
     });
